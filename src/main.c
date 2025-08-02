ab823537842526c2fc23f39d9a3405432dd234e2
Eli <elijah2307.williams@gmail.com>
2025-08-02 12:26:02 +0100

    added basic command parsing with cd and ls support

--- src/main.c
+++ src/main.c
@@ -43,9 +43,6 @@ void print_command_line(){
         free(tempStr);
     }
 
-    
-
-
     printf("\n%s\n", currDir);
     free(currDir);
 }
@@ -79,8 +76,67 @@ int get_input(char* str){
     return 1;
 }
 
-int parse_input(char* str){
+int changeDirectory(char** arguments, int numberArguments){
+    if(numberArguments!=1){
+        printf("Too many arguments\n");
+        return 1;
+    }
+    chdir(arguments[0]);
     return 0;
+}
+
+int ls(){
+   system("ls"); 
+}
+
+int handleArguments(char** arguments, int numberArguments){
+    
+    char* shellCommands[1];
+    shellCommands[0]= "cd";
+    if(strcmp(arguments[0], shellCommands[0]) == 0 ){
+        changeDirectory(arguments+1,numberArguments-1);
+    }
+    else if(strcmp(arguments[0],"ls") == 0){
+        ls();
+    }
+    else{
+        printf("did not find command");
+    }
+
+    return 0;
+}
+
+int parse_input(char* input){
+    char* token;
+    const char delimiter[4] = " ";
+    token = strtok(input, delimiter);
+    int numberArguments=0;
+    int size = 20;
+    char** arguments = (char**)malloc(size * sizeof(char*));
+    while(token != 0){
+        printf(" %s\n",token);
+        if(numberArguments+1>=size){
+            size=size*1.5;
+            char** tmp=arguments; 
+            arguments = (char**)realloc(tmp, size*sizeof(char*));
+            printf("reallocated new size:%d\n",size);
+        }
+        arguments[numberArguments] = token;
+        numberArguments++;
+        
+        token= strtok(0, delimiter);
+    }
+
+    for(int j=0; j<numberArguments;j++){
+        printf("%s,",arguments[j]);
+    }
+    printf("\n");
+
+
+    //logic for handling input
+    
+    return handleArguments(arguments, numberArguments);
+
 }
 
 int main(){
@@ -95,7 +151,7 @@ int main(){
         }
         //add input to history
         add_history(input_command);
-        printf("input received: %s",input_command);
+        printf("input received: %s\n",input_command);
 
         //parse input separate command and args 
         res = parse_input(input_command); 
